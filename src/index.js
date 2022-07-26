19f39b20427b1666a01482e4f0707557c71b85d0
for-ashraf <46405183+for-ashraf@users.noreply.github.com>
2022-07-27 00:01:08 +0500

    Display data from API
    Display of API data has been implemented

--- src/index.js
+++ src/index.js
@@ -1,37 +1,11 @@
 import './css/style.css';
 
-import logo from './images/logo.png';
+import ShowController from './js/controllers/displaycontroller.js';
 
-import ShowController from './js/controllers/showscontroller.js';
-
-import CommentsController from './js/controllers/commentsController.js';
-
-const logoDiv = document.querySelector('.logo');
-const myLogo = new Image();
-myLogo.src = logo;
-logoDiv.appendChild(myLogo);
 const shows = new ShowController([]);
+
 const start = 100;
-const end = 200;
+
+const end = 120;
+
 shows.fetchRange(start, end);
-const commentPopupHandler = () => {
-    if (shows.moviesArray.length === end - start) {
-      const allCommentButtons = document.querySelectorAll('.comment-button');
-      allCommentButtons.forEach((button) => {
-        button.addEventListener('click', () => {
-          const commentPopup = new CommentsController(shows, button);
-          commentPopup.render();
-  
-          const postComment = document.querySelector('.commentPopup-button');
-          postComment.addEventListener('click', () => {
-            commentPopup.sendComment();
-          });
-        });
-      });
-    } else {
-      setTimeout(commentPopupHandler, 15);
-    }
-  };
-  
-  commentPopupHandler();
-  
