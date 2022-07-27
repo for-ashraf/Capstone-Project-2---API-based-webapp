209f76c5701a8ce35454fad6913cf32cc58c7224
unknown <abrahak81@gmail.com>
2022-07-27 21:40:33 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,33 +1,12 @@
 import './css/style.css';
 
 import ShowController from './js/controllers/displaycontroller.js';
-import ReservationsController from './js/controllers/reservationsController.js';
+
 
 const shows = new ShowController([]);
 
 const start = 100;
 
-const end = 120;
+const end = 110;
 
 shows.fetchRange(start, end);
-const reservationPopupHandler = () => {
-  if (shows.moviesArray.length === end - start) {
-    const allReservationButtons = document.querySelectorAll('.btn-secondary');
-
-    allReservationButtons.forEach((button) => {
-      button.addEventListener('click', () => {
-        const reservationPopup = new ReservationsController(shows, button);
-        reservationPopup.render();
-
-        const postReservation = document.querySelector('.commentPopup-button');
-        postReservation.addEventListener('click', () => {
-          reservationPopup.sendReservation();
-        });
-      });
-    });
-  } else {
-    setTimeout(reservationPopupHandler, 15);
-  }
-};
-
-reservationPopupHandler();
